f6f5d2daab0db85598ffc3eaca61db1384bc8d98
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2022-11-17 10:32:59 +0500

    feat(guard): roles base authorization

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 import { CatsService } from './cats.service';
+import { Roles } from '../decorator/roles.decorator';
 import { CreateCatDto, ListAllEntities, UpdateCatDto } from './dto/cats.dto';
+import { Role } from 'src/decorator/role.enum';
 
+@Roles(Role.USER)
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
+
   @Post()
+  @Roles(Role.ADMIN)
   async create(@Body() createCatDto: CreateCatDto) {
     return this.catsService.create(createCatDto);
   }
--- src/guards/logger.guards.ts
+++ src/guards/logger.guards.ts
@@ -1,10 +1,20 @@
 import { Observable } from 'rxjs';
 import { Request } from 'express';
 import { CanActivate, ExecutionContext, Injectable, Logger } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'src/decorator/role.enum';
+
+const validationObject = {
+  '1xzye': { role: Role.ADMIN },
+  '2xzye': { role: Role.USER },
+  '3xzye': { role: Role.USER },
+};
 
 @Injectable()
 export class AuthGaurd implements CanActivate {
   private logger = new Logger(AuthGaurd.name);
+
+  constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const ctx = context.switchToHttp();
@@ -12,8 +22,16 @@ export class AuthGaurd implements CanActivate {
     const className = context.getClass().name;
     const request = ctx.getRequest<Request>();
 
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [context.getHandler(), context.getClass()]);
+
     const { ip, path, method } = request;
     this.logger.log(`${AuthGaurd.name} ${method} ${path} ${ip} ${className} ${handler}`);
-    return true;
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const authUserRole = validationObject[request.headers.authorization]?.role;
+    return requiredRoles.includes(authUserRole);
   }
 }
